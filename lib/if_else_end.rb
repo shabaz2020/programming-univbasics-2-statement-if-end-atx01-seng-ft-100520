1d32974d2ebb555fe9f8bbb9ed874071313d71c9
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-08 17:25:36 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,7 @@
-time.authencurrent_time = Time.now
+current_time = Time.now
 current_time = current_time.to_i
  
-if current_time.even?
+if current_time % 2 == 0
   puts "Even!"
 else
   puts "Odd!"
